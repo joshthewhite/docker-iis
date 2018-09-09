c62e4e78def3dd93defbaa17973c35c8fbdffb4d
Josh White <joshjwhite@gmail.com>
2018-09-08 23:07:26 -0500

    fixed cygwin bash script

--- rebuild-docker.sh
+++ rebuild-docker.sh
@@ -3,5 +3,5 @@
 docker stop iis-site && docker rm iis-site
 #docker rmi iis-image
 docker build -t iis-image .
-docker run -d -p 8000:8000 -v d:/workspace/docker-iis/content:c:/site --name iis-site iis-image
+docker run -d -p 8000:8000 -v $(cygpath -w $(pwd))/content:c:/site --name iis-site iis-image
 docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" iis-site
